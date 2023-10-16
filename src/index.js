19e8f9fbabeb614cfa78dcded9da49ec865b7ec6
cynthia-creator <141678093+cynthia-creator@users.noreply.github.com>
2023-10-16 11:12:03 +0200

    chsnged http to htpps

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function showWeatherForecast(response) {
      `<div class="col-2">
     <div class="weather-forecast-date">
     ${formatDay(forecastDay.dt)}</div>
-    <img src="http://openweathermap.org/img/
+    <img src="https://openweathermap.org/img/
     wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"
                            />
                   <div class="weather-forecast-temperatures">
